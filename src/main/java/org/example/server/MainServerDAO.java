90a50a7556e22977b36e783b818a08284e0b07e4
saidaniamine12 <mohamedaminesaidani7@gmail.com>
2023-10-05 01:50:34 +0100

    "first commit"

--- src/main/java/org/example/server/MainServerDAO.java
+++ src/main/java/org/example/server/MainServerDAO.java
@@ -1,5 +1,9 @@
 package org.example.server;
 
+import org.example.models.Player;
+import org.example.models.Sprite;
+import org.example.models.StartGameInfo;
+
 import java.io.IOException;
 
 public interface MainServerDAO {
@@ -7,4 +11,24 @@ public interface MainServerDAO {
     void start() throws IOException;
 
     void processClient(SocketMainServerDAO.ClientSession clientSession);
+
+    void writeStartGameInfo(StartGameInfo startGameInfo);
+
+    void sendMoveLeft() throws IOException;
+
+    void sendMoveRight() throws IOException;
+
+    void sendMoveUp() throws IOException;
+
+    void sendMoveDown() throws IOException;
+
+    void sendBomb() throws IOException;
+
+    void sendOk() throws IOException;
+
+    void sendKo() throws IOException;
+
+    void writePlayer(Player player) throws IOException;
+
+    void writeSprite(Sprite sprite) throws IOException;
 }
--- src/main/java/org/example/server/SocketMainServerDAO.java
+++ src/main/java/org/example/server/SocketMainServerDAO.java
@@ -1,5 +1,11 @@
 package org.example.server;
 
+import org.example.models.DynamicGameModel;
+import org.example.models.Player;
+import org.example.models.Sprite;
+import org.example.models.StartGameInfo;
+import org.example.models.enums.ProtocolConstants;
+
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
@@ -11,6 +17,9 @@ import java.util.Map;
 public class SocketMainServerDAO implements MainServerDAO{
 
     private  ServerSocket serverSocket;
+
+    private ObjectInputStream objectInputStream;
+    private ObjectOutputStream objectOutputStream;
     private final static int port = 4444;
     private Map<Integer,ClientSession> playerToSocketMap = new HashMap<>();
 
@@ -24,13 +33,19 @@ public class SocketMainServerDAO implements MainServerDAO{
             while (true){
                 Socket clientSocket = serverSocket.accept();
 
-                ObjectInputStream objectInputStream = new ObjectInputStream(clientSocket.getInputStream());
-                ObjectOutputStream objectOutputStream = new ObjectOutputStream(clientSocket.getOutputStream());
+                this.objectInputStream = new ObjectInputStream(clientSocket.getInputStream());
+                this.objectOutputStream = new ObjectOutputStream(clientSocket.getOutputStream());
 
-                Long playerId = objectInputStream.readLong();
+                Integer playerId = objectInputStream.readInt();
                 ClientSession clientSession = new ClientSession(clientSocket,objectInputStream,objectOutputStream,playerId);
-                playerToSocketMap.put(playerId.intValue(),clientSession);
-                processClient(clientSession);
+                playerToSocketMap.put(playerId,clientSession);
+                Thread thread = new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Runnable runnable = () -> processClient(clientSession);
+                    }
+                });
+                thread.start();
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -42,17 +57,107 @@ public class SocketMainServerDAO implements MainServerDAO{
 
     @Override
     public void processClient(ClientSession clientSession) {
+        try {
+            ProtocolConstants isConnect = (ProtocolConstants) this.objectInputStream.readObject();
+            if (isConnect == ProtocolConstants.CONNECT) {
+                System.out.println("Client connected");
+                StartGameInfo startGameInfo = new StartGameInfo();
+                Player player = (Player) this.objectInputStream.readObject();
+                this.objectOutputStream.writeObject(startGameInfo);
+                playerToSocketMap.put(player.getId(),clientSession);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void writeStartGameInfo(StartGameInfo startGameInfo)  {
+        try {
+            this.objectOutputStream.writeObject(startGameInfo);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
+
+    @Override
+    public void sendMoveLeft() throws IOException {
+        ProtocolConstants left = ProtocolConstants.LEFT;
+        this.objectOutputStream.write(left.getValue());
+        this.objectOutputStream.flush();
+    }
+
+    @Override
+    public void sendMoveRight() throws IOException {
+        ProtocolConstants right = ProtocolConstants.RIGHT;
+        this.objectOutputStream.write(right.getValue());
+        this.objectOutputStream.flush();
+    }
+
+    @Override
+    public void sendMoveUp() throws IOException {
+        ProtocolConstants up = ProtocolConstants.UP;
+        this.objectOutputStream.write(up.getValue());
+        this.objectOutputStream.flush();
+    }
+
+    @Override
+    public void sendMoveDown() throws IOException {
+        ProtocolConstants down = ProtocolConstants.DOWN;
+        this.objectOutputStream.write(down.getValue());
+        this.objectOutputStream.flush();
+    }
+
+    @Override
+    public void sendBomb() throws IOException {
+        ProtocolConstants bomb = ProtocolConstants.BOMB;
+        this.objectOutputStream.write(bomb.getValue());
+        this.objectOutputStream.flush();
+    }
+
+    @Override
+    public void sendOk() throws IOException {
+        ProtocolConstants ok = ProtocolConstants.OK;
+        this.objectOutputStream.write(ok.getValue());
+        this.objectOutputStream.flush();
+    }
+
+    @Override
+    public void sendKo() throws IOException {
+        ProtocolConstants ko = ProtocolConstants.KO;
+        this.objectOutputStream.write(ko.getValue());
+        this.objectOutputStream.flush();
+    }
+
+    @Override
+    public void writePlayer(Player player) throws IOException {
+        this.objectOutputStream.writeObject(player);
+        this.objectOutputStream.flush();
+    }
+
+    @Override
+    public void writeSprite(Sprite sprite) throws IOException {
+        this.objectOutputStream.writeObject(sprite);
+        this.objectOutputStream.flush();
+    }
+
+    public void sendModelChanged(DynamicGameModel dynamicGameModel) throws IOException {
+        this.objectOutputStream.writeObject(dynamicGameModel);
+        this.objectOutputStream.flush();
+    }
+
 
     public class ClientSession {
         private Socket clientSocket;
         private ObjectInputStream objectInputStream;
         private ObjectOutputStream objectOutputStream;
-        private Long playerId;
+        private Integer playerId;
         public ClientSession() {
         }
-        public ClientSession(Socket clientSocket, ObjectInputStream objectInputStream, ObjectOutputStream objectOutputStream, Long playerId) {
+        public ClientSession(Socket clientSocket, ObjectInputStream objectInputStream, ObjectOutputStream objectOutputStream, Integer playerId) {
             this.clientSocket = clientSocket;
             this.objectInputStream = objectInputStream;
             this.objectOutputStream = objectOutputStream;
